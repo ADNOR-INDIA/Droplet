643f3515bfab378320a75c16cba8160bb12e9c03
Abhay Gupta <adnorindia@gmail.com>
2022-07-11 00:17:13 +0530

    cookies added

--- client/src/components/AuthModal.js
+++ client/src/components/AuthModal.js
@@ -1,23 +1,43 @@
 import React, {useState} from "react";
+import axios from 'axios'
+import {useNavigate} from "react-router-dom"
+import {useCookies} from 'react-cookie'
+
 
 const AuthModal = ({setShowModal, isSignUp} )=>{
+
+    const navigate = useNavigate()
 
     const [email, setEmail] = useState(null)
     const [password, setPassword] = useState(null)
     const [confirmPassword, setConfirmPassword] = useState(null)
     const [error, setError] = useState(null)
+    const [cookies, setCookie, removeCookie] = useCookies(['user'])
 
-    console.log(email, password, confirmPassword)
+    //console.log(email, password, confirmPassword)
     const handleClick=()=>{
         setShowModal(false)
     }
-    const handleSubmit=(e)=>{
+    const handleSubmit=async(e)=>{
         e.preventDefault()
         try{
             if(isSignUp&&(password!==confirmPassword)){
                 setError("password does not match confirm password")
+                return
             }
-            console.log('make a post request to our database')
+            // console.log('make a post request to our database')
+            // Making a post request to our database, where we are passing email and password only.
+            //console.log('posting', email, password)
+            const response = await axios.post('http://localhost:5000/signup', {email, password})
+
+            setCookie('Email', response.data.email)
+            setCookie('UserId', response.data.userId)
+            setCookie('AuthToken', response.data.token)
+
+            const success = response.status===201
+            if(success){
+                navigate('/onboarding')
+            }
         }
         catch(error){
             console.log(error)
